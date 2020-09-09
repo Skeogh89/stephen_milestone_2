1dea8c24995439c1f8552fd411244e1c5df2a421
Stephen Keogh <stevokeogh@hotmail.com>
2020-09-09 13:40:49 +0000

    added starting postion for game in JS

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/* current postion of the game*/
+
+var stephen = function () {
+
+    stephenPosition = (function() {
+        var positions = {
+            off: 0 , 
+            on: 1 , 
+            start: 2, 
+
+
+        }
+    })
+    
+}
