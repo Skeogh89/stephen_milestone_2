56d725d413a8e0378ff3bd6dcb4c0dfbcbc307bd
Stephen Keogh <stevokeogh@hotmail.com>
2020-09-14 10:26:20 +0000

    added postion of game object in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,32 @@ var stephen = function () {
             off: 0 , 
             on: 1 , 
             start: 2, 
+            addRadom: 3,
+            sequence: 4,
+            playerSelection: 5,
+            incorrect: 6,
+            correct: 7,
+            win: 8,
+          };
+        var postion = positions.off;
 
+        var returnObject = {
+            getState: function() {
+                return postion;
+        },
+        setState: function (i) {
+            console.log ('position' + i);
+            if (i >= 0 && i <= postions.win) {
+                postion = i;
+            }
+        },
+        positions: positions,
+        };
 
-        }
-    })
-    
-}
+        for (var k in positions)
+            returnObject[k] = positions[k];
+        
+        return returnObject;
+
+        })();
+    }
